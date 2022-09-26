2687753f22074ef85fae7f8a25ca787409cd862b
Brad Russell <brad.russell@e-gineering.com>
2022-09-26 13:00:39 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { foods } from "./food";
+import "./index.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">React Restaurant</h1>
+      <div className="flex flex-wrap">
+        {foods.map((food) => {
+          return (
+            <div className="max-w-sm cursor-pointer border-solid border-2 border-indigo-600 rounded-lg p-2 m-2 shadow-md hover:bg-indigo-600 hover:text-white">
+              <h2 className="font-semibold text-lg">{food.name}</h2>
+              <img
+                className="h-52"
+                src={`/images/${food.image}`}
+                alt={food.name}
+              />
+              <p>{food.description}</p>
+              <p>${food.price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}

70dc87571f504809c6d474f160a1d28ddc27eb44
Brad Russell <brad.russell@e-gineering.com>
2022-09-26 14:50:15 -0400

    Enhance sizing logic

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -11,7 +11,7 @@ export default function Menu() {
               key={food.name}
               className="max-w-sm cursor-pointer border-solid border-2 border-indigo-600 rounded-lg p-2 m-2 shadow-md hover:bg-indigo-600 hover:text-white"
             >
-              <h2 className="font-semibold text-lg">{food.name}</h2>
+              <Heading level={4}>{food.name}</Heading>
               <img
                 className="h-52"
                 src={`/images/${food.image}`}
--- src/shared/Heading.tsx
+++ src/shared/Heading.tsx
@@ -3,11 +3,20 @@ type HeadingProps = {
   children: React.ReactNode;
   level: 1 | 2 | 3 | 4 | 5 | 6;
 };
+
+const sizeMap = {
+  1: "text-4xl",
+  2: "text-3xl",
+  3: "text-2xl",
+  4: "text-xl",
+  5: "text-lg",
+  6: "text-base",
+};
+
 export default function Heading(prop: HeadingProps) {
   const Tag = `h${prop.level}` as keyof JSX.IntrinsicElements;
   return (
-    <Tag className="font-bold text-xl p-2 text-slate-600">
-      <ForwardIcon className="h-4 w-4 mb-1 mr-2 inline-block" />
+    <Tag className={`font-bold ${sizeMap[prop.level]} p-2 text-slate-600`}>
       <span className="inline-block">{prop.children}</span>
     </Tag>
   );

6031711a33e29721f88ca7f5d39891f4e0054528
Brad Russell <brad.russell@e-gineering.com>
2022-09-28 11:22:50 -0400

    Add unit tests for Button

---
+++ src/shared/Button.test.tsx
@@ -0,0 +1,39 @@
+import Button from "./Button";
+import { render, screen } from "@testing-library/react";
+
+describe("Button", () => {
+  it("should render children", () => {
+    render(<Button type="button">Click me</Button>);
+    expect(screen.getByRole("button", { name: "Click me" }));
+  });
+
+  it("should set a custom css class", () => {
+    render(
+      <Button type="button" className="custom">
+        Click me
+      </Button>
+    );
+    expect(screen.getByRole("button", { name: "Click me" })).toHaveClass(
+      "custom"
+    );
+  });
+
+  it("should set the correct type prop", () => {
+    render(<Button type="submit">Click me</Button>);
+    expect(screen.getByRole("button", { name: "Click me" })).toHaveAttribute(
+      "type",
+      "submit"
+    );
+  });
+
+  it("should set the button variant", () => {
+    render(
+      <Button type="button" variant="primary">
+        Click me
+      </Button>
+    );
+    expect(screen.getByRole("button", { name: "Click me" })).toHaveClass(
+      "bg-blue-500"
+    );
+  });
+});

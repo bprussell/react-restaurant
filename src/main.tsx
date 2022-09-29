41492622de580c113459e1c444e310471ca5a2eb
Brad Russell <brad.russell@e-gineering.com>
2022-09-29 10:48:19 -0400

    Add mockServiceWorker

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ErrorBoundary } from "react-error-boundary";
+import { worker } from "./mocks/browser";
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

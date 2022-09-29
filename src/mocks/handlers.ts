b686d86f676b0275901bc15a0edc50992ceae8d6
Brad Russell <brad.russell@e-gineering.com>
2022-09-29 11:02:54 -0400

    Implement mock foods

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -12,26 +12,37 @@ export const handlers = [
     );
   }),
 
-  rest.get("/user", (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem("is-authenticated");
-
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: "Not authorized",
-        })
-      );
-    }
-
-    // If authenticated, return a mocked user details
+  rest.get("http://localhost:3001/foods", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        username: "admin",
-      })
+      ctx.json([
+        {
+          id: 1,
+          name: "Burger",
+          image: "burger.jpg",
+          price: 8.99,
+          description:
+            "This ain't your average burger. Topped with our tangy cheddar cheese sauce, fresh lettuce, and tomato.",
+          tags: ["Lunch", "Dinner"],
+        },
+        {
+          id: 2,
+          name: "Banana Blueberry French Toast",
+          image: "banana-french-toast.jpg",
+          price: 9.99,
+          description: "Delicious french toast with banana and blueberry.",
+          tags: ["Breakfast"],
+        },
+        {
+          id: 3,
+          name: "Cajun Pasta",
+          image: "cajun-pasta.jpg",
+          price: 16.99,
+          description:
+            "Creole-style pasta that's guaranteed to make you sweat.",
+          tags: ["Lunch", "Dinner", "Spicy"],
+        },
+      ])
     );
   }),
 ];
